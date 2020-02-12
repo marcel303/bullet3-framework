8a888d4da8eda34bc75b96565f7e0e864dd99168
Marcel Smit <marcel303@gmail.com>
2020-02-12 21:25:31 +0100

    - Add capsule shape generation. - Add multi-sphere shape generation. - Add compound shape -> box, sphere generation.

--- FrameworkGuiHelper.cpp
+++ FrameworkGuiHelper.cpp
@@ -45,88 +45,100 @@ void FrameworkGUIHelperInterface::createCollisionObjectGraphicsObject(btCollisio
 
 #include "OpenGLWindow/ShapeData.h"
 
+static void appendPrim(
+	const float * vertices,
+	const int numVertices,
+	const int * indices,
+	const int numIndices,
+	const btTransform * transform,
+	const btVector3 & halfExtents,
+	btAlignedObjectArray<float> & out_vertices,
+	btAlignedObjectArray<int> & out_indices)
+{
+	const int baseVertex = out_vertices.size() / 9;
+	
+	out_vertices.resize(out_vertices.size() + numVertices * 9);
+	for (int i = 0; i < numVertices * 9; ++i)
+		out_vertices[baseVertex*9 + i] = vertices[i];
+
+	for (int i = 0; i < numVertices; ++i)
+	{
+		float & x = out_vertices[(baseVertex + i) * 9 + 0];
+		float & y = out_vertices[(baseVertex + i) * 9 + 1];
+		float & z = out_vertices[(baseVertex + i) * 9 + 2];
+		
+		x *= halfExtents.x();
+		y *= halfExtents.y();
+		z *= halfExtents.z();
+		
+		if (transform != nullptr)
+		{
+			const btVector3 p = (*transform) * btVector3(x, y, z);
+			
+			x = p.x();
+			y = p.y();
+			z = p.z();
+		}
+	}
+	
+	const int baseIndex = out_indices.size();
+	out_indices.resize(out_indices.size() + numIndices);
+	for (int i = 0; i < numIndices; ++i)
+		out_indices[baseIndex + i] = baseVertex + indices[i];
+}
+
 void FrameworkGUIHelperInterface::createCollisionShapeGraphicsObject(btCollisionShape* collisionShape)
 {
 	// already has a graphics object ?
+	
 	if (collisionShape->getUserIndex() >= 0)
 		return;
 	
 	const int vertexStrideInBytes = 9 * sizeof(float);
+
+	btAlignedObjectArray<float> out_vertices;
+	btAlignedObjectArray<int> out_indices;
 	
 	if (collisionShape->getShapeType() == BOX_SHAPE_PROXYTYPE)
 	{
 		const btBoxShape * boxShape = (btBoxShape*)collisionShape;
 		const btVector3 halfExtents = boxShape->getHalfExtentsWithMargin();
 		
-		const int numVertices = sizeof(cube_vertices_textured) / vertexStrideInBytes;
-		const int numIndices = sizeof(cube_indices) / sizeof(cube_indices[0]);
-		const int * indices = cube_indices;
-		
-		btAlignedObjectArray<float> transformedVertices;
-		transformedVertices.resize(numVertices * 9);
-		for (size_t i = 0; i < transformedVertices.size(); ++i)
-			transformedVertices[i] = cube_vertices_textured[i];
-		
-		for (int i = 0; i < numVertices; ++i)
-		{
-			transformedVertices[i * 9 + 0] *= halfExtents.x();
-			transformedVertices[i * 9 + 1] *= halfExtents.y();
-			transformedVertices[i * 9 + 2] *= halfExtents.z();
-		}
-		
-		const int graphicsShapeId = m_renderInterface->registerShape(
-			&transformedVertices[0],
-			numVertices,
-			indices,
-			numIndices,
-			B3_GL_TRIANGLES,
-			0);
-
-		collisionShape->setUserIndex(graphicsShapeId);
+		appendPrim(
+			cube_vertices_textured,
+			sizeof(cube_vertices_textured) / vertexStrideInBytes,
+			cube_indices,
+			sizeof(cube_indices) / sizeof(cube_indices[0]),
+			nullptr,
+			halfExtents,
+			out_vertices,
+			out_indices);
 	}
 	else if (collisionShape->getShapeType() == SPHERE_SHAPE_PROXYTYPE)
 	{
 		const btSphereShape * sphereShape = (btSphereShape*)collisionShape;
-		const btScalar radius = sphereShape->getRadius();
-		const btScalar sphereSize = 2. * radius;
-		const btVector3 radiusScale(sphereSize, sphereSize, sphereSize);
+		const btScalar sphereSize = 2. * sphereShape->getRadius();
 
-		const int numVertices = sizeof(textured_detailed_sphere_vertices) / vertexStrideInBytes;
-		const int numIndices = sizeof(textured_detailed_sphere_indices) / sizeof(textured_detailed_sphere_indices[0]);
-		const int * indices = textured_detailed_sphere_indices;
-		
-		btAlignedObjectArray<float> transformedVertices;
-		transformedVertices.resize(numVertices * 9);
-		for (size_t i = 0; i < transformedVertices.size(); ++i)
-			transformedVertices[i] = textured_detailed_sphere_vertices[i];
-		
-		for (int i = 0; i < numVertices; ++i)
-		{
-			transformedVertices[i * 9 + 0] *= radiusScale.x();
-			transformedVertices[i * 9 + 1] *= radiusScale.y();
-			transformedVertices[i * 9 + 2] *= radiusScale.z();
-		}
-		
-		const int graphicsShapeId = m_renderInterface->registerShape(
-			&transformedVertices[0],
-			numVertices,
-			indices,
-			numIndices,
-			B3_GL_TRIANGLES,
-			0);
-
-		collisionShape->setUserIndex(graphicsShapeId);
+		appendPrim(
+			textured_detailed_sphere_vertices,
+			sizeof(textured_detailed_sphere_vertices) / vertexStrideInBytes,
+			textured_detailed_sphere_indices,
+			sizeof(textured_detailed_sphere_indices) / sizeof(textured_detailed_sphere_indices[0]),
+			nullptr,
+			btVector3(sphereSize, sphereSize, sphereSize),
+			out_vertices,
+			out_indices);
 	}
 	else if (collisionShape->getShapeType() == CAPSULE_SHAPE_PROXYTYPE)
 	{
 		const btCapsuleShape * capsuleShape = (btCapsuleShape*)collisionShape;
-		int up = capsuleShape->getUpAxis();
-		btScalar halfHeight = capsuleShape->getHalfHeight();
+		const int up = capsuleShape->getUpAxis();
+		const btScalar halfHeight = capsuleShape->getHalfHeight();
 
-		btScalar radius = capsuleShape->getRadius();
-		btScalar sphereSize = 2. * radius;
+		const btScalar radius = capsuleShape->getRadius();
+		const btScalar sphereSize = 2. * radius;
 
-		btVector3 radiusScale = btVector3(sphereSize, sphereSize, sphereSize);
+		const btVector3 radiusScale = btVector3(sphereSize, sphereSize, sphereSize);
 
 		const int numVertices = sizeof(textured_detailed_sphere_vertices) / vertexStrideInBytes;
 		const int numIndices = sizeof(textured_detailed_sphere_indices) / sizeof(textured_detailed_sphere_indices[0]);
@@ -159,15 +171,84 @@ void FrameworkGUIHelperInterface::createCollisionShapeGraphicsObject(btCollision
 
 		collisionShape->setUserIndex(graphicsShapeId);
 	}
-	
-	/*
-	if (collisionShape->getShapeType() == MULTI_SPHERE_SHAPE_PROXYTYPE)
-	if (collisionShape->getShapeType() == COMPOUND_SHAPE_PROXYTYPE)
-	if (collisionShape->getShapeType() == CAPSULE_SHAPE_PROXYTYPE)
+	else if (collisionShape->getShapeType() == MULTI_SPHERE_SHAPE_PROXYTYPE)
 	{
-		// todo : textured_detailed_sphere_vertices;
+		const btMultiSphereShape * multiSphereShape = (btMultiSphereShape*)collisionShape;
+		
+		for (int i = 0; i < multiSphereShape->getSphereCount(); ++i)
+		{
+			const btVector3 position = multiSphereShape->getSpherePosition(i);
+			const btScalar radius = multiSphereShape->getSphereRadius(i);
+			const btScalar sphereSize = 2. * radius;
+
+			btTransform transform;
+			transform.setIdentity();
+			transform.setOrigin(position);
+			
+			appendPrim(
+				textured_detailed_sphere_vertices,
+				sizeof(textured_detailed_sphere_vertices) / vertexStrideInBytes,
+				textured_detailed_sphere_indices,
+				sizeof(textured_detailed_sphere_indices) / sizeof(textured_detailed_sphere_indices[0]),
+				&transform,
+				btVector3(sphereSize, sphereSize, sphereSize),
+				out_vertices,
+				out_indices);
+		}
 	}
-	*/
+	else if (collisionShape->getShapeType() == COMPOUND_SHAPE_PROXYTYPE)
+	{
+		const btCompoundShape * compound = (btCompoundShape*)collisionShape;
+		
+		for (int i = 0; i < compound->getNumChildShapes(); ++i)
+		{
+			const btCollisionShape * childShape = compound->getChildShape(i);
+			
+			if (childShape->getShapeType() == BOX_SHAPE_PROXYTYPE)
+			{
+				const btBoxShape * boxShape = (btBoxShape*)childShape;
+				const btVector3 halfExtents = boxShape->getHalfExtentsWithMargin();
+				
+				appendPrim(
+					cube_vertices_textured,
+					sizeof(cube_vertices_textured) / vertexStrideInBytes,
+					cube_indices,
+					sizeof(cube_indices) / sizeof(cube_indices[0]),
+					&compound->getChildTransform(i),
+					halfExtents,
+					out_vertices,
+					out_indices);
+			}
+			else if (childShape->getShapeType() == SPHERE_SHAPE_PROXYTYPE)
+			{
+				const btSphereShape * sphereShape = (btSphereShape*)childShape;
+				const btScalar sphereSize = 2.f * sphereShape->getRadius();
+
+				appendPrim(
+					textured_detailed_sphere_vertices,
+					sizeof(textured_detailed_sphere_vertices) / vertexStrideInBytes,
+					textured_detailed_sphere_indices,
+					sizeof(textured_detailed_sphere_indices) / sizeof(textured_detailed_sphere_indices[0]),
+					&compound->getChildTransform(i),
+					btVector3(sphereSize, sphereSize, sphereSize),
+					out_vertices,
+					out_indices);
+			}
+		}
+	}
+	
+	if (out_indices.size() > 0)
+	{
+		const int graphicsShapeId = m_renderInterface->registerShape(
+			&out_vertices[0],
+			out_vertices.size() / 9,
+			&out_indices[0],
+			out_indices.size(),
+			B3_GL_TRIANGLES,
+			0);
+
+		collisionShape->setUserIndex(graphicsShapeId);
+	}
 }
 
 void FrameworkGUIHelperInterface::syncPhysicsToGraphics(const btDiscreteDynamicsWorld* rbWorld)
