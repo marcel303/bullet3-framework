072a8a7c59b6164556e1658c3d86d87b1cc896f5
Marcel Smit <marcel303@gmail.com>
2020-02-13 14:37:19 +0100

    Gui polish.

--- FrameworkExampleBrowser.cpp
+++ FrameworkExampleBrowser.cpp
@@ -435,6 +435,81 @@ void FrameworkExampleBrowser::update(float deltaTime)
 
 //
 
+static void doParameterInterface(FrameworkParameterInterface * paramInterface)
+{
+	for (auto & param : paramInterface->m_params)
+	{
+		switch (param.type)
+		{
+		case FrameworkParameterInterface::kParamType_Undefined:
+			break;
+			
+		case FrameworkParameterInterface::kParamType_Button:
+			if (param.button.m_isTrigger && false)
+			{
+				if (ImGui::Button(param.button.m_name))
+				{
+					if (param.button.m_callback != nullptr)
+						param.button.m_callback(param.button.m_buttonId, 1, param.button.m_userPointer);
+				}
+			}
+			else
+			{
+				bool value = param.value != 0.f;
+				
+				if (ImGui::Selectable(param.button.m_name, &value))
+				{
+					param.value = value ? 1.f : 0.f;
+					if (param.button.m_callback != nullptr)
+						param.button.m_callback(param.button.m_buttonId, param.button.m_initialState, param.button.m_userPointer);
+				}
+			}
+			break;
+		case FrameworkParameterInterface::kParamType_Slider:
+			{
+				if (param.slider.m_clampToIntegers)
+				{
+					int value = *param.slider.m_paramValuePointer;
+					if (ImGui::SliderInt(param.slider.m_name, &value, param.slider.m_minVal, param.slider.m_maxVal))
+					{
+						param.value = value;
+						if (param.slider.m_callback != nullptr)
+							param.slider.m_callback(value, param.slider.m_userPointer);
+					}
+				}
+				else
+				{
+					float value = *param.slider.m_paramValuePointer;
+					if (ImGui::SliderFloat(param.slider.m_name, &value, param.slider.m_minVal, param.slider.m_maxVal))
+					{
+						param.value = value;
+						if (param.slider.m_callback != nullptr)
+							param.slider.m_callback(value, param.slider.m_userPointer);
+					}
+				}
+			}
+			break;
+		case FrameworkParameterInterface::kParamType_ComboBox:
+			{
+				ImGui::PushID(&param);
+				int item = (int)param.value;
+				if (ImGui::Combo("Combo", &item, param.comboBox.m_items, param.comboBox.m_numItems))
+				{
+					if (param.comboBox.m_callback != nullptr)
+					{
+						param.comboBox.m_callback(
+							param.comboBox.m_comboboxId,
+							param.comboBox.m_items[(int)param.value],
+							param.comboBox.m_userPointer);
+					}
+				}
+				ImGui::PopID();
+			}
+			break;
+		}
+	}
+}
+
 static void doImGui(FrameworkImGuiContext & guiContext)
 {
 	guiContext.processBegin(framework.timeStep, s_app->m_window->getWidth(), s_app->m_window->getHeight(), inputIsCaptured);
@@ -478,103 +553,53 @@ static void doImGui(FrameworkImGuiContext & guiContext)
 				
 				ImGui::EndMenu();
 			}
+			
+			auto * paramInterface = static_cast<FrameworkParameterInterface*>(s_app->m_parameterInterface);
+			
+			if (paramInterface->m_params.empty() == false)
+			{
+				if (ImGui::BeginMenu("Parameters"))
+				{
+					doParameterInterface(paramInterface);
+					
+					ImGui::EndMenu();
+				}
+			}
 		}
 		ImGui::EndMainMenuBar();
 		
-		ImGui::SetNextWindowCollapsed(true, ImGuiCond_Once);
+		int viewSx;
+		int viewSy;
+		framework.getCurrentViewportSize(viewSx, viewSy);
+		
+		//ImGui::SetNextWindowCollapsed(true, ImGuiCond_Once);
+		ImGui::SetNextWindowPos(ImVec2(10, viewSy*3/5), ImGuiCond_Once);
+		ImGui::SetNextWindowSize(ImVec2(0, viewSy*1/3), ImGuiCond_Once);
+		ImGui::SetNextWindowBgAlpha(.5f);
 		if (ImGui::Begin("Example Browser"))
 		{
+			static char filter[32];
+			ImGui::InputText("Filter", filter, sizeof(filter));
+			
 			for (int i = 0; i < gAllExamples->getNumRegisteredExamples(); ++i)
 			{
 				const char * name = gAllExamples->getExampleName(i);
 				const auto & createFunc = gAllExamples->getExampleCreateFunc(i);
 				
+				if (filter[0] != 0 && strcasestr(name, filter) == nullptr)
+					continue;
+				
 				if (createFunc != nullptr)
 				{
-					if (ImGui::Button(name))
+					ImGui::Indent();
+					if (ImGui::Selectable(name))
+					//if (ImGui::Button(name))
 						selectDemo(i);
+					ImGui::Unindent();
 				}
 				else
 				{
 					ImGui::Text("%s", name);
-				}
-			}
-		}
-		ImGui::End();
-		
-		if (ImGui::Begin("Parameters", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
-		{
-			auto * paramInterface = static_cast<FrameworkParameterInterface*>(s_app->m_parameterInterface);
-			
-			for (auto & param : paramInterface->m_params)
-			{
-				switch (param.type)
-				{
-				case FrameworkParameterInterface::kParamType_Undefined:
-					break;
-					
-				case FrameworkParameterInterface::kParamType_Button:
-					if (param.button.m_isTrigger)
-					{
-						if (ImGui::Button(param.button.m_name))
-						{
-							if (param.button.m_callback != nullptr)
-								param.button.m_callback(param.button.m_buttonId, 1, param.button.m_userPointer);
-						}
-					}
-					else
-					{
-						bool value = param.value != 0.f;
-						
-						if (ImGui::Checkbox(param.button.m_name, &value))
-						{
-							param.value = value ? 1.f : 0.f;
-							if (param.button.m_callback != nullptr)
-								param.button.m_callback(param.button.m_buttonId, param.button.m_initialState, param.button.m_userPointer);
-						}
-					}
-					break;
-				case FrameworkParameterInterface::kParamType_Slider:
-					{
-						if (param.slider.m_clampToIntegers)
-						{
-							int value = *param.slider.m_paramValuePointer;
-							if (ImGui::SliderInt(param.slider.m_name, &value, param.slider.m_minVal, param.slider.m_maxVal))
-							{
-								param.value = value;
-								if (param.slider.m_callback != nullptr)
-									param.slider.m_callback(value, param.slider.m_userPointer);
-							}
-						}
-						else
-						{
-							float value = *param.slider.m_paramValuePointer;
-							if (ImGui::SliderFloat(param.slider.m_name, &value, param.slider.m_minVal, param.slider.m_maxVal))
-							{
-								param.value = value;
-								if (param.slider.m_callback != nullptr)
-									param.slider.m_callback(value, param.slider.m_userPointer);
-							}
-						}
-					}
-					break;
-				case FrameworkParameterInterface::kParamType_ComboBox:
-					{
-						ImGui::PushID(&param);
-						int item = (int)param.value;
-						if (ImGui::Combo("Combo", &item, param.comboBox.m_items, param.comboBox.m_numItems))
-						{
-							if (param.comboBox.m_callback != nullptr)
-							{
-								param.comboBox.m_callback(
-									param.comboBox.m_comboboxId,
-									param.comboBox.m_items[(int)param.value],
-									param.comboBox.m_userPointer);
-							}
-						}
-						ImGui::PopID();
-					}
-					break;
 				}
 			}
 		}
